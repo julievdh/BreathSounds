b84e981e4e59891df1da6131b4382fe2e46cc291
julievdh <au575532@uni.au.dk>
2018-05-25 09:41:35 +0200

    Add text, legend, etc

--- BreathCounts/BelugaSurfacingPlots.m
+++ BreathCounts/BelugaSurfacingPlots.m
@@ -8,16 +8,25 @@ R = loadaudit(tag);
 surf = findaudit(R,'surf'); 
 resph = findaudit(R,'resph');
 
-figure(1)
+figure(1), clf
 % plot mean inter-surface interval
-subplot(121),
+h1 = subplot(121);
 histogram(diff(resph(:,1)))
 xlabel('Interval (s)')
 ylabel('Count')
+text(0.6*h1.XLim(2),0.9*h1.YLim(2),['mean: ' num2str(mean(diff(resph(:,1))))])
+text(0.6*h1.XLim(2),0.85*h1.YLim(2),['median: ' num2str(median(diff(resph(:,1))))])
+
 
 % plot duration of those surfacings 
-subplot(122), hold on
+h2 = subplot(122); hold on
 histogram(resph(:,2))
 histogram(surf(:,2))
+legend('Breath','Surfacing')
 xlabel('Duration (s)')
+ylabel('Count')
+text(0.6*h2.XLim(2),0.8*h2.YLim(2),['mean: ' num2str(mean(resph(:,2)))])
+text(0.6*h2.XLim(2),0.75*h2.YLim(2),['median: ' num2str(median(resph(:,2)))])
+text(0.6*h2.XLim(2),0.7*h2.YLim(2),['mean: ' num2str(mean(surf(:,2)))])
+text(0.6*h2.XLim(2),0.65*h2.YLim(2),['median: ' num2str(median(surf(:,2)))])
 
