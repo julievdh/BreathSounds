369d6e32863f31d010e7134f297bbdcf80aba5f6
julievdh <au575532@uni.au.dk>
2018-05-29 15:20:11 +0200

    Gray whale audits from Paolo

--- BreathCounts/PaoloAuditConvert.m
+++ BreathCounts/PaoloAuditConvert.m
@@ -2,7 +2,7 @@
 clear 
 % convert .csv files from Paolo to tag audits
 % load csv 
-tag = 'bw170813-44';
+tag = 'er160505-22';
 fs = 10; 
 
 ftoread = strcat('c:/tag/audit/',tag,'prh10_breaths.csv');
@@ -15,13 +15,13 @@ M = textscan(fid, '%f', 'Delimiter',','); % you will need to change the number  
 fclose (fid);
 
 %% remove bad sections or CEE 
-bad = 92650+(36*60*60*fs); % end of CEE + 24h 
-ii = find(M{1,1}(:,1) > bad); 
-M{1,1} = M{1,1}(ii,1); 
+%bad = 92650+(36*60*60*fs); % end of CEE + 24h 
+%ii = find(M{1,1}(:,1) > bad); 
+%M{1,1} = M{1,1}(ii,1); 
 
 %% put in audit structure
 R.cue = M{1,1}/fs; % convert, as audit is in samples, not seconds
-(R.cue(end,1)-R.cue(1,1))/3600 % check duration 
+(R.cue(end,1)-R.cue(1,1))/3600 % check duration with spreadsheet
 R.stype = cell(1,length(M{1,1})); 
 R.stype(:) = {'breath'}; 
 
