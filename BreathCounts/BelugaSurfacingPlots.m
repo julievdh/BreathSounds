8b9b57bf14c505882598e7b921498b182421b7c6
julievdh <au575532@uni.au.dk>
2018-06-07 09:35:00 +0200

    separate resps and surfs, remove those within, plot

--- BreathCounts/BelugaSurfacingPlots.m
+++ BreathCounts/BelugaSurfacingPlots.m
@@ -1,24 +1,32 @@
 %% beluga audit data example for manolo
 
 % load tag
-tag = 'dl16_135a';
+tag = 'dl16_138a';
 R = loadaudit(tag);
 
 % find surfacings 
 surf = findaudit(R,'surf'); 
 resph = findaudit(R,'resph');
 
+% find any resphs within surfacings
+wthn = iswithin(resph(:,1),[surf(:,1) surf(:,1)+surf(:,2)]);
+uresph = resph(~wthn,:); % unique resps (not those within surfs)
+
+allsrf = vertcat(surf,uresph); % combine
+allsrf = sortrows(allsrf,1); % sort
+
 figure(1), clf
 % plot mean inter-surface interval
 h1 = subplot(121);
-histogram(diff(resph(:,1)))
+histogram(diff(allsrf(:,1)))
 xlabel('Interval (s)')
 ylabel('Count')
-text(0.6*h1.XLim(2),0.9*h1.YLim(2),['mean: ' num2str(mean(diff(resph(:,1))))])
-text(0.6*h1.XLim(2),0.85*h1.YLim(2),['median: ' num2str(median(diff(resph(:,1))))])
+title(regexprep(tag,'_','  '))
+text(0.6*h1.XLim(2),0.9*h1.YLim(2),['mean: ' num2str(mean(diff(allsrf(:,1))))])
+text(0.6*h1.XLim(2),0.85*h1.YLim(2),['median: ' num2str(median(diff(allsrf(:,1))))])
 
 
-% plot duration of those surfacings 
+% plot duration of breaths and surfacings 
 h2 = subplot(122); hold on
 histogram(resph(:,2))
 histogram(surf(:,2))
@@ -30,7 +38,7 @@ text(0.6*h2.XLim(2),0.75*h2.YLim(2),['median: ' num2str(median(resph(:,2)))])
 text(0.6*h2.XLim(2),0.7*h2.YLim(2),['mean: ' num2str(mean(surf(:,2)))])
 text(0.6*h2.XLim(2),0.65*h2.YLim(2),['median: ' num2str(median(surf(:,2)))])
 
-print([cd '\BreathCounts\RespSurfDistribution_' 'tag'],'-dpng','-r300')
+print([cd '\BreathCounts\RespSurfDistribution_' tag],'-dpng','-r300')
 
 %% requested from Manolo: interval between midpoints of consecutive surfacings
 
@@ -51,8 +59,10 @@ mdpt = cues(:,1)+cues(:,2)/2;
 intrvl = diff(mdpt); 
 
 % plot
-figure(3), subplot(2,4,k)
+figure(3), h2 = subplot(2,4,k);
 histogram(intrvl,0:10:400)
 title(regexprep(tag,'_','  '))
+text(0.5*h2.XLim(2),0.8*h2.YLim(2),['mean: ' num2str(mean(intrvl))])
+text(0.5*h2.XLim(2),0.65*h2.YLim(2),['median: ' num2str(median(intrvl))])
 
 end
