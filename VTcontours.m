7199506cb61ec81222238ee2dbf5811f54da2e83
julievdh <au575532@uni.au.dk>
2018-06-08 15:28:47 +0200

    Add free-swimming breaths

--- VTcontours.m
+++ VTcontours.m
@@ -26,3 +26,33 @@ xlabel('Duration (s)'), ylabel('Mean flow rate (L/s)')
 h = colorbar; 
 ylabel(h,'VT estimate error (L)')
 
+%% plot all free-swimming estimated breaths too
+figure(121), clf, hold on 
+q20 = find(Quality == 20); % find all free-swimming breaths
+
+% compute mean estimated flow rate
+for i = 1:length(surfstore)
+    mnflowIest(i)= nanmean(surfstore(i).Festi); 
+end
+
+scatter(ins(q20,3),abs(mnflowIest),20)
+
+
+% range of durations
+drange = 0:0.05:max([allstore(:).din]);
+% range of flows
+frange = 0:1:50;
+
+xlim([min(drange) max(drange)])
+
+VTcontour5 = repmat(5,1,length(frange))./frange;
+VTcontour7 = repmat(7,1,length(frange))./frange; 
+VTcontour10 = repmat(10,1,length(frange))./frange; 
+
+plot(VTcontour5,frange,'color',[0.75 0.75 0.75])
+plot(VTcontour7,frange,'color',[0.75 0.75 0.75])
+plot(VTcontour10,frange,'color',[0.75 0.75 0.75])
+
+xlabel('Duration (s)'), ylabel('Mean flow rate (L/s)')
+h = colorbar; 
+ylabel(h,'VT estimate error (L)')
